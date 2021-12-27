902885134803aa62d59afbbbe234c4df2ae543f0
chengyuanzhao <chengyuanzhao@haohaozhu.com>
2021-12-27 16:35:25 +0800

    初始自己的组件库

---
+++ src/Calc.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Yxz\ComposerTest;
+
+class Calc
+{
+    // 计算平方值
+    public function square($val)
+    {
+        return $val * $val;
+    }
+}
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e0d309b070d9289420434b09c5ac0b5d3bb9cd06',
+    'name' => 'cyz/composer_test',
+  ),
+  'versions' => 
+  array (
+    'cyz/composer_test' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e0d309b070d9289420434b09c5ac0b5d3bb9cd06',
+    ),
+  ),
+);
